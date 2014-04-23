0752c415c8384c84922a9376db5271396ae5c1cc
CristianDuta <cristian.duta@acrux.biz>
2014-04-23 14:09:46 +0300

    directory structure

---
+++ QuestionGenerator/index.php
@@ -0,0 +1,18 @@
+<?php
+	//todo
+?>
+
+<!doctype html>
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Quotezone Question Generator</title>
+</head>
+
+<body>
+  <!-- //todo -->
+</body>
+
+</html>
