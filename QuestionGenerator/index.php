228f6311a3e01bf8e73cbb11613cd995b6f22b13
Rodica90 <Rodica90@users.noreply.github.com>
2014-04-25 16:12:30 +0300

    added initial design

--- QuestionGenerator/index.php
+++ QuestionGenerator/index.php
@@ -1,18 +1,91 @@
-<?php
-	//todo
-?>
-
 <!doctype html>
 
 <html lang="en">
-
+<!-- [Start Head] -->
 <head>
-  <meta charset="utf-8">
-  <title>Quotezone Question Generator</title>
+    <meta charset="UTF-8">
+    <title>Question generator</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script type="text/javascript" src=""></script>
+    <script type="text/javascript" src=""></script>
 </head>
+<!-- [End Head] -->
 
+<!-- [Start Body] -->
 <body>
-  <!-- //todo -->
+<div id="siteContent" class="siteContent">
+    <!-- start header -->
+    <div class="sectionHeader">
+        <div id="header">
+            <a href="http://www.quotezone.co.uk/" title="Quotezone Home" rel="home" id="logo">
+                <img src="css/images/logo.png" alt="Quotezone Home">
+            </a>
+        </div>
+    </div>
+    <!-- end header -->
+
+    <!-- start center -->
+    <div class="sectionCenter">
+        <div class="leftSection">
+
+            <br clear="all"/>
+        </div>
+        <div class="rightSection">
+            <form action="" method="get" name="" id="">
+                <div class="rowClass">
+                    <p class="rowTitle">Question Details</p>
+
+                    <div class="rowContent">
+                        <div class="rowQuestion">
+                            <p class="labelText">Name:</p>
+                            <input type="text" name="">
+                            <br clear="all"/>
+                        </div>
+                        <div class="rowQuestion">
+                            <p class="labelText">Session Entry:</p>
+                            <input type="text" name="">
+                            <br clear="all"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="rowClass">
+                    <p class="rowTitle">Question Generator</p>
+
+                    <div class="rowContent">
+                        <div class="generatedQuestionContent">
+                            <input class="titleInputClass" type="text" name="" value="Title..." readonly disabled="true">
+                        </div>
+                        <div class="optionPanel">
+                            <button type="button">Save Question</button>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="rowClass titleInputRow">
+                    <p class="rowTitle">Question Title</p>
+                    <div class="rowContent">
+                        <input type="text" name="test">
+                        <br clear="all" />
+                        <button class='margin10' type="button" name="saveTitle">Save Title</button>
+                    </div>
+                </div>
+            </form>
+            <br clear="all"/>
+        </div>
+        <br clear="all"/>
+    </div>
+    <br clear="all"/>
+    <!-- end center -->
+
+    <!-- start footer -->
+    <div class="sectionFooter">
+    </div>
+    <!-- end footer -->
+
+</div>
+
+
 </body>
+<!-- [End Body] -->
 
 </html>
